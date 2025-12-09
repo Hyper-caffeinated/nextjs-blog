f3243acb53ec507ef0756b91d4ef9b3413016330
tomarc753 <thomascrosno79@gmail.com>
2025-12-08 20:30:23 -0500

    testing
    Added a comment to clarify the purpose of assetPrefix.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/nextjs-blog",
-  assetPrefix: "/nextjs-blog",
+  assetPrefix: "/nextjs-blog", // test: should be isProd ? '/<repository-name>' : '',
   output: "export",
   reactStrictMode: true,
   images: {
