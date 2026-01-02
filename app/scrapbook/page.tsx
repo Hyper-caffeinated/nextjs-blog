62a4e73b45576283c57dc2347777e1dfe4fd8225
Hyper-caffeinated <156276098+Hyper-caffeinated@users.noreply.github.com>
2026-01-01 19:44:33 -0500

    Update page.tsx
    Change scrapbook entries

--- app/scrapbook/page.tsx
+++ app/scrapbook/page.tsx
@@ -2,12 +2,11 @@ import { HeroBanner } from "../components/HeroBanner";
 import React from "react";
 
 const Entries = [
-  { imageUrl: "https://i.imgur.com/njHroyT.jpeg" , caption: "Duck 1", author: "Thomas", date: "4/2/2025" },
-  { imageUrl: "https://i.imgur.com/njHroyT.jpeg" , caption: "Duck 2", author: "Also Thomas", date: "4/2/2025"}
+  { imageUrl: "https://i.imgur.com/njHroyT.jpeg" , caption: "Canada Geese", author: "Thomas Crosno", date: "4/14/2025", loc: "Seattle, WA" },
 ]
 
 // date will go with author (ex: "mat bu, Jan 2024")
-const ScrapbookEntry = ({ imageUrl, caption, author, date }: { imageUrl: string; caption: string; author: string; date: string }) => (
+const ScrapbookEntry = ({ imageUrl, caption, author, date, loc }: { imageUrl: string; caption: string; author: string; date: string, loc: string }) => (
   <div className="m-4 p-4 border-2 border-stone-600 bg-white rounded-lg shadow-lg max-w-xs">
     <img
       src={ imageUrl }
@@ -16,7 +15,7 @@ const ScrapbookEntry = ({ imageUrl, caption, author, date }: { imageUrl: string;
       className="w-full h-64 object-cover rounded-md"
     />
     <p className="mt-3 text-center text-sm text-stone-800 font-serif font-medium">{ caption }</p>
-    <p className="mt-3 text-center text-sm text-stone-600 font-serif font-medium">{ author }, { date }</p>
+    <p className="mt-3 text-center text-sm text-stone-600 font-serif font-medium">{ author }, { date } at { loc }</p>
   </div>
 );
 
@@ -33,8 +32,8 @@ const Scrapbook = () => {
         <div className="container mx-auto px-6">
           { Entries.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 justify-center">
-              {Entries.map(({ imageUrl, caption, author, date }, index) => (
-                <ScrapbookEntry key={ index } imageUrl={ imageUrl } caption={ caption } author={ author } date={ date }/>
+              {Entries.map(({ imageUrl, caption, author, date, loc }, index) => (
+                <ScrapbookEntry key={ index } imageUrl={ imageUrl } caption={ caption } author={ author } date={ date } loc={ loc }/>
               ))}
             </div>
           ) : (
